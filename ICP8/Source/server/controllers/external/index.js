c7598db73b2c414a32a6d958d9f5f4d6a7cd8945
Anvesh <anveshmandadi821@gmail.com>
2018-10-15 03:24:57 -0500

    ICP8

--- ICP8/Source/server/controllers/external/index.js
+++ ICP8/Source/server/controllers/external/index.js
@@ -1,7 +1,6 @@
 var request = require('request');
 module.exports = function(app) {
     var query='https://language.googleapis.com/v1beta2/documents:analyzeSyntax?key=AIzaSyDJ2wCKzrrHqHAD3faZM0rrxuZY0faIMWU';
-    var url = 'https://api.wunderground.com/api/36b799dc821d5836/hourly/q/MO/kansas%20city.json';
     app.post('/anvesh/nlp', function(req, res) {
         console.log('hello');
        request.post({

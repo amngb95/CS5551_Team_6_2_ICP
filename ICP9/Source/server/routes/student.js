6746ffaac20805e5845c29be778d061290bc0c21
Anvesh <anveshmandadi821@gmail.com>
2018-10-21 01:01:09 -0500

    ICP9

--- ICP9/Source/server/routes/student.js
+++ ICP9/Source/server/routes/student.js
@@ -1,23 +1,5 @@
 module.exports = function (app, db) {
     let student_details = db.model('student');
-    app.get('/student',(req,res)=>{
-        let class_id = req.query.classid;
-        student_details.find({ class_id : class_id }).exec((err, students) => {
-            if (!err) {
-                res.send({
-                    result: "Success",
-                    data: students
-                });
-            } else {
-                res.send({
-                    result: "Failure",
-                    message: "Error in fetching students",
-                    error: err
-                });
-            }
-        });
-    });
-
     app.get('/student/search',(req,res)=>{
         let search_text = req.query.major;
         console.log(search_text);
@@ -40,6 +22,7 @@ module.exports = function (app, db) {
 
     app.post('/student/create',(req,res) => {
         let student = req.body;
+        console.log(student);
         let stud_details = new student_details({
             class_id: student.studentId ,
             student_name: student.studentName,
